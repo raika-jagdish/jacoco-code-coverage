79e98febac88cb08ab7af0592c628d2c2fd67b00
raikajagdish <raika.jagdish@gmail.com>
2019-07-24 17:48:25 +0530

    shell deleted

--- start.sh
+++
@@ -1,8 +0,0 @@
-mvn clean install
-mvn spring-boot:run
-
-curl -X GET http://localhost:8080/test/9
-curl -X GET http://localhost:8080/test/50
-
-java -jar src/main/resources/lib/jacococli.jar report target/jacoco-it.exec --classfiles target/classes/com --sourcefiles src/main/java/ --html target/report
-java -jar src/main/resources/lib/jacococli.jar report target/jacoco-it.exec --classfiles target/classes/com --sourcefiles src/main/java/ --html target/report
