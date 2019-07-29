1b52cb85f3b868f48d649a9f94e550a770a32310
raika-jagdish <raika.jagdish@gmail.com>
2019-07-29 12:39:31 +0530

    start.sh updated

--- jacoco-code-coverage-example/start.sh
+++ jacoco-code-coverage-example/start.sh
@@ -1,3 +1,4 @@
+export MAVEN_OPTS=-javaagent:src/main/resources/lib/jacocoagent.jar=address=*,ort=36320,destfile=jacoco-it.exec,output=tcpserver
 mvn clean install
 mvn spring-boot:run
 
@@ -5,4 +6,4 @@ curl -X GET http://localhost:8080/test/9
 curl -X GET http://localhost:8080/test/50
 
 java -jar src/main/resources/lib/jacococli.jar report target/jacoco-it.exec --classfiles target/classes/com --sourcefiles src/main/java/ --html target/report
-java -jar src/main/resources/lib/jacococli.jar report target/jacoco-it.exec --classfiles target/classes/com --sourcefiles src/main/java/ --html target/report
+java -jar src/main/resources/lib/jacococli.jar report target/jacoco-it.exec --classfiles target/classes/com --sourcefiles src/main/java/ --html target/report 
